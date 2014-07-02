25c5390eda729e82f929200bead33e503768a632
Quinn Romanek <quinnromanek@gmail.com>
2014-07-01 22:04:42 -0700

    Began work on REST API - pt 2

--- dynasty/urls.py
+++ dynasty/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import patterns, url
-
+from django.conf.urls import patterns, url, include
+from tastypie.api import Api
+from dynasty.api import *
 from dynasty import views
+
+beta_api = Api(api_name="beta")
+beta_api.register(PlayerResource())
+beta_api.register(TeamResource())
+beta_api.register(GameResource())
+beta_api.register(PlayerStatResource())
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
@@ -8,6 +15,7 @@ urlpatterns = patterns('',
                        url(r'^teams/', views.teams),
                        url(r'^players/', views.players),
                        url(r'^games/(?P<game_id>\d+)', views.game),
-                       url(r'^games/', views.games)
+                       url(r'^games/', views.games),
+                       url(r'^service/', include(beta_api.urls))
 
 )
