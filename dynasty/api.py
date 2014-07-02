65f34e12a77d6fe087c46ed73e7834c3ead5a13d
Quinn Romanek <quinnromanek@gmail.com>
2014-07-01 22:03:58 -0700

    Began work on REST API

---
+++ dynasty/api.py
@@ -0,0 +1,50 @@
+from tastypie.constants import ALL
+
+__author__ = 'flex109'
+
+from tastypie.resources import ModelResource
+from dynasty.models import *
+from tastypie import fields
+
+
+class TeamResource(ModelResource):
+    class Meta:
+        queryset = Team.objects.all()
+        filtering = {
+            "name": 'exact',
+            "division": 'exact'
+        }
+        ordering = ['name', 'wins', 'division']
+
+class PlayerResource(ModelResource):
+    team = fields.ForeignKey(TeamResource, 'team')
+
+    class Meta:
+        queryset = Player.objects.all()
+        filtering = {
+
+        }
+        ordering = ['name', 'age', 'defense',
+                    'offense', 'athletics', 'primary_position',
+                    'secondary_position', 'team', 'roster',
+                    'minutes']
+
+
+class GameResource(ModelResource):
+
+    away_team = fields.ForeignKey(TeamResource, 'away_team')
+    home_team = fields.ForeignKey(TeamResource, 'home_team')
+
+    class Meta:
+        queryset = Game.objects.all()
+
+
+class PlayerStatResource(ModelResource):
+    player = fields.ForeignKey(PlayerResource, 'player')
+    game = fields.ForeignKey(GameResource, 'game')
+    team = fields.ForeignKey(TeamResource, 'team')
+
+    class Meta:
+        queryset = PlayerStats.objects.all()
+
+
